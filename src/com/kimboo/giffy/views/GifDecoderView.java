d311dc558086aab2e9fe1941ea5c954aecc397a2
Agustin Tomas Larghi <astinx@localhost.localdomain>
2013-08-19 00:22:31 -0300

    Navigation

--- src/com/kimboo/giffy/views/GifDecoderView.java
+++ src/com/kimboo/giffy/views/GifDecoderView.java
@@ -1,12 +1,16 @@
 
 package com.kimboo.giffy.views;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.widget.ImageView;
+
+import com.kimboo.giffy.R;
 
 public class GifDecoderView extends ImageView {
 
@@ -26,42 +30,84 @@ public class GifDecoderView extends ImageView {
         }
     };
 
-    public GifDecoderView(Context context, InputStream stream) {
+
+    private String filepath;
+
+    private InputStream stream;
+
+
+    public GifDecoderView(Context context) {
         super(context);
-        playGif(stream);
     }
 
-    private void playGif(InputStream stream) {
-        mGifDecoder = new GifDecoder();
-        mGifDecoder.read(stream);
+    public void playGif(String filepath) {
+        setFilepath(filepath);
+        setBackgroundDrawable(getResources().getDrawable(R.drawable.tv_loading));
+        new Player().execute(filepath);
+    }
+    
+    private InputStream getInputStream(Context context, String filepath) {
+        InputStream stream = null;
+        try {
+            stream = context.getAssets().open(filepath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return stream;
+    }
+    
+    class Player extends AsyncTask<String, Void, Void> {
 
-        mIsPlayingGif = true;
+        @Override
+        protected Void doInBackground(String... params) {
+            mGifDecoder = new GifDecoder();
+            stream = getInputStream(getContext(), getFilepath());
+            mGifDecoder.read(stream);
+            mIsPlayingGif = true;
+            return null;
+        }
 
-        new Thread(new Runnable() {
-            public void run() {
-                final int n = mGifDecoder.getFrameCount();
-                final int ntimes = mGifDecoder.getLoopCount();
-                int repetitionCounter = 0;
-                do {
-                    for (int i = 0; i < n; i++) {
-                        mTmpBitmap = mGifDecoder.getFrame(i);
-                        int t = mGifDecoder.getDelay(i);
-                        mHandler.post(mUpdateResults);
-                        try {
-                            Thread.sleep(t);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            new Thread(new Runnable() {
+                public void run() {
+                    final int n = mGifDecoder.getFrameCount();
+                    final int ntimes = mGifDecoder.getLoopCount();
+                    int repetitionCounter = 0;
+                    do {
+                        for (int i = 0; i < n; i++) {
+                            mTmpBitmap = mGifDecoder.getFrame(i);
+                            int t = mGifDecoder.getDelay(i);
+                            mHandler.post(mUpdateResults);
+                            try {
+                                Thread.sleep(t);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
                         }
-                    }
-                    if(ntimes != 0) {
-                        repetitionCounter ++;
-                    }
-                } while (mIsPlayingGif && (repetitionCounter <= ntimes));
-            }
-        }).start();
+                        if(ntimes != 0) {
+                            repetitionCounter ++;
+                        }
+                    } while (mIsPlayingGif && (repetitionCounter <= ntimes));
+                }
+            }).start();
+        }
+        
+        
+        
     }
     
     public void stopRendering() {
         mIsPlayingGif = true;
+    }
+
+
+    public String getFilepath() {
+        return filepath;
+    }
+
+    public void setFilepath(String filepath) {
+        this.filepath = filepath;
     }
 }
